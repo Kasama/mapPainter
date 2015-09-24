abe247de8b449b626b5231665eccecc791501759
Kasama <ralegro@bol.com.br>
2015-09-24 17:41:34 -0300

    added map reader

--- src/map_reader.rb
+++ src/map_reader.rb
@@ -1,2 +1,41 @@
 class MapReader
+  def self.read_map(filename)
+=begin
+     all lines in the file follow /^.+:.*\.$/
+     ^ = beginning of line
+     .+ = any character 1 or more times
+     : = ':' literally
+     .* = any character 0 or more times
+     \. = '.' literally
+     $ = end of line
+=end
+    # open file to read
+    file = open filename, 'r'
+
+    # initialize the graph
+    graph = Hash.new do |hash, key|
+      # each entry have a empty array to start with
+      hash[key] = Array.new
+    end
+
+    # for each file line
+    file.each_line do |l|
+      # ignore any lines with numbers in it or empty
+      next if l.to_s.match /\d+.*/ or l.to_s.match /^\s*$/
+      # set the node_name to what comes before the ':' and neighbors to what comes after it
+      node_name, neighbors = l.to_s.match(/^(.+):(.*).$/i).captures
+      # turn the string into a symbol without leading/following spaces
+      node_sym = node_name.strip.to_sym
+      # touch the graph with the node name to create an array for it
+      graph[node_sym]
+      # split the neighbors
+      neighbors = neighbors.split(', ')
+      #add each neighbor to the array
+      neighbors.each do |n| graph[node_sym].push n end
+    end
+
+    # returns the graph
+    graph
+
+  end
 end
